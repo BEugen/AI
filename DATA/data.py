700a5c9aff35d0135c6f69effad739ec8cd398fc
BattsEA <b-eugen@yandex.ru>
2017-09-14 17:19:19 +0300

    Create learn

--- DATA/data.py
+++ DATA/data.py
@@ -1,6 +1,9 @@
 from DATA import datasql
 from METHEO import htmltotable
+from CNTK import  CNTKCassification
 import datetime
+import time
+import re
 
 sql = datasql.GetDataFromPc('sa', 'cproject', '172.31.185.41', 'Runtime')
 proxies = {
@@ -9,12 +12,21 @@ proxies = {
 }
 obj = htmltotable.HtmlTables('http://rp5.ru/%D0%90%D1%80%D1%85%D0%B8%D0%B2_%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D1%8B_%D0'
                              '%B2_%D0%9D%D0%B8%D0%BA%D0%B5%D0%BB%D0%B5', proxies)
+hour = -1
 while True:
     dt = datetime.datetime.now()
-    hour = -1
     if dt.hour != hour:
-        hour = dt.hour
-        table = obj.read()
+        try:
+            hour = dt.hour
+            table = obj.read()[8]
+            series = table.Series[1]
+            year = re.search(r'(?P<year>\d+)', '').group('year')
+            day_month = re.search(r'(?P<dmyear>\d+ \w+)', '').group('dm')
+            print(table)
+        except Exception as exp:
+            print(exp)
 
     if dt.second % 10 == 0:
         data = sql.read()
+
+    time.sleep(1)
