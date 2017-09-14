ec597ad1f89ddbb262c659f299ecce451ef3e23c
eugen <b-eugen@yandex.ru>
2017-09-14 20:38:33 +0300

    cntk test

--- DATA/data.py
+++ DATA/data.py
@@ -3,6 +3,7 @@ from METHEO import htmltotable
 from CNTK import  CNTKCassification
 import datetime
 import time
+import locale
 import re
 
 sql = datasql.GetDataFromPc('sa', 'cproject', '172.31.185.41', 'Runtime')
@@ -11,7 +12,7 @@ proxies = {
     "https": "127.0.0.1:3128",
 }
 obj = htmltotable.HtmlTables('http://rp5.ru/%D0%90%D1%80%D1%85%D0%B8%D0%B2_%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D1%8B_%D0'
-                             '%B2_%D0%9D%D0%B8%D0%BA%D0%B5%D0%BB%D0%B5', proxies)
+                             '%B2_%D0%9D%D0%B8%D0%BA%D0%B5%D0%BB%D0%B5', None)
 hour = -1
 while True:
     dt = datetime.datetime.now()
@@ -20,8 +21,14 @@ while True:
             hour = dt.hour
             table = obj.read()[8]
             series = table.iloc[1]
-            year = re.search(r'(?P<year>\d+)', series[0]).group('year')
-            day_month = re.search(r'(?P<dm>\d+ \w+)', series[0]).group('dm')
+            dats = series[0]
+            year = re.search(r'(?P<year>\d+)', dats).group('year')
+            day_month = re.search(r'(?P<dm>\d+\W\w+)', dats).group('dm')
+            d = re.search(r'(?P<d>\d+)', day_month).group('d')
+            m = re.search(r'(?P<m>\D+\W*)', day_month).group('m')[1:4]
+            h = re.search(r'(?P<d>\d*)', series[1]).group('d')
+            locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
+            result = datetime.datetime.strptime(d + '-' + m + '-' + year + ' ' + h + ':00', u'%d-%b-%Y %H:%M')
             print(table)
         except Exception as exp:
             hour = -1
