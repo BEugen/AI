84b0fd89c98c221d7719cb32cd53a92b3b744398
eugen <b-eugen@yandex.ru>
2017-09-14 22:11:54 +0300

    cntk test

--- DATA/data.py
+++ DATA/data.py
@@ -11,8 +11,20 @@ proxies = {
     "http": "127.0.0.1:3128",
     "https": "127.0.0.1:3128",
 }
+
+wnd = {'Ветер, дующий с востока': 90, 'Ветер, дующий с востоко-северо-востока': 68,
+       'Ветер, дующий с востоко-юго-востока': 112, 'Ветер, дующий с запада': 270,
+       'Ветер, дующий с западо-северо-запада': 292, 'Ветер, дующий с западо-юго-запада': 248,
+       'Ветер, дующий с севера': 360, 'Ветер, дующий с северо-востока': 45,
+       'Ветер, дующий с северо-запада': 298, 'Ветер, дующий с северо-северо-востока': 22,
+       'Ветер, дующий с северо-северо-запада': 318, 'Ветер, дующий с юга': 180,
+       'Ветер, дующий с юго-востока': 135, 'Ветер, дующий с юго-запада': 225,
+       'Ветер, дующий с юго-юго-востока': 112, 'Ветер, дующий с юго-юго-запада': 202,
+       'Штиль, безветрие': 0, '': 0}
+
 obj = htmltotable.HtmlTables('http://rp5.ru/%D0%90%D1%80%D1%85%D0%B8%D0%B2_%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D1%8B_%D0'
                              '%B2_%D0%9D%D0%B8%D0%BA%D0%B5%D0%BB%D0%B5', None)
+sql = datasql.SqlLiteBase('/home/eugen/PycharmProjects/WebSOVisu/db.sqlite3')
 hour = -1
 while True:
     dt = datetime.datetime.now()
@@ -29,7 +41,18 @@ while True:
             h = re.search(r'(?P<d>\d*)', series[1]).group('d')
             locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
             result = datetime.datetime.strptime(d + '-' + m + '-' + year + ' ' + h + ':00', u'%d-%b-%Y %H:%M')
-            print(table)
+            wd = dict()
+            wd['wth_date'] = result
+            wd['T'] = float(re.search(r'(?P<d>\d*.\d*)', series[2]).group('d'))
+            wd['Po'] = float(re.search(r'(?P<d>\d*.\d*)', series[3]).group('d'))
+            wd['P'] = float(re.search(r'(?P<d>\d*.\d*)', series[4]).group('d'))
+            wd['U'] = float(re.search(r'(?P<d>\d*)', series[6]).group('d'))
+            wd['ff10'] = float(re.search(r'(?P<d>\d*)', series[9]).group('d'))
+            wd['ff3'] = float(re.search(r'(?P<d>\d*)', series[10]).group('d'))
+            wd['Td'] = float(re.search(r'(?P<d>\d*.\d*)', series[23]).group('d'))
+            wd['RRR'] = float(re.search(r'(?P<d>\d*.\d*)', series[24]).group('d'))
+            wd['Wg'] = wnd[series[7]]
+            sql.writewheterdata(wd)
         except Exception as exp:
             hour = -1
             print(exp)
--- DATA/datasql.py
+++ DATA/datasql.py
@@ -16,41 +16,41 @@ class GetDataFromPc(object):
             self.login + ';PWD=' + self.psw)
         cursor = cnxn.cursor()
         cursor.execute('select GetDate(), (SELECT AVG([Value]) FROM Runtime.dbo.v_AnalogHistory'
-                        ' WHERE [TagName] = \'Converter4_Q\'  AND [DateTime] > DATEADD(mi, -2, GetDate()) AND'
-                        '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\') as c4_q,'
-                        '(SELECT AVG([Value]) FROM Runtime.dbo.v_AnalogHistory'
-                        ' WHERE [TagName] = \'Converter5_Q\'  AND [DateTime] > DATEADD(mi, -2, GetDate()) AND'
-                        '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\') as c5_q,'
-                        '(SELECT AVG([Value]) FROM Runtime.dbo.v_AnalogHistory'
-                        ' WHERE [TagName] = \'Converter6_Q\'  AND [DateTime] > DATEADD(mi, -2, GetDate()) AND'
-                        '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\') as c6_q,'
-                        '(SELECT AVG([Value]) FROM Runtime.dbo.v_AnalogHistory'
-                        ' WHERE [TagName] = \'Converter7_Q\'  AND [DateTime] > DATEADD(mi, -2, GetDate()) AND'
-                        '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\') as c7_q,'
-                        '(SELECT AVG([Value]) FROM Runtime.dbo.v_AnalogHistory'
-                        ' WHERE [TagName] = \'Converter8_Q\'  AND [DateTime] > DATEADD(mi, -2, GetDate()) AND'
-                        '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\') as c8_q,'
-                        '(SELECT AVG([Value]) FROM Runtime.dbo.v_AnalogHistory'
-                        ' WHERE [TagName] = \'Flue_SO2_Molodejniy\'  AND [DateTime] > DATEADD(mi, -2, GetDate()) AND'
-                        '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\') as SO2_M,'
-                        '(SELECT AVG([Value]) FROM Runtime.dbo.v_AnalogHistory'
-                        ' WHERE [TagName] = \'Flue_SO2_Nagornaya\'  AND [DateTime] > DATEADD(mi, -2, GetDate()) AND'
-                        '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\') as SO2_N,'
-                        '(SELECT AVG([Value]) FROM Runtime.dbo.v_AnalogHistory'
-                        ' WHERE [TagName] = \'Flue_SO2_UGMS\'  AND [DateTime] > DATEADD(mi, -2, GetDate()) AND'
-                        '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\') as SO2_UG,'
-                        '(SELECT TOP 1 CAST([Value] as int) & 67108864 FROM Runtime.dbo.v_AnalogHistory'
-                        ' WHERE [TagName] = \'ShiberLocalRemote_RTP3\'  AND'
-                        '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\' order by '
-                        '[DateTime] desc) as RTP3,'
-                        '(SELECT TOP 1 CAST([Value] as int) & 67108864 FROM Runtime.dbo.v_AnalogHistory'
-                        ' WHERE [TagName] = \'ShiberLocalRemote_RTP4\'  AND'
-                        '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\' order by '
-                        '[DateTime] desc) as RTP4,'
-                        '(SELECT TOP 1 CAST([Value] as int) & 1 FROM Runtime.dbo.v_AnalogHistory'
-                        ' WHERE [TagName] = \'P5_mess_Furnace\' AND'
-                        '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\' order by '
-                        '[DateTime] desc) as RTP5')
+                       ' WHERE [TagName] = \'Converter4_Q\'  AND [DateTime] > DATEADD(mi, -2, GetDate()) AND'
+                       '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\') as c4_q,'
+                       '(SELECT AVG([Value]) FROM Runtime.dbo.v_AnalogHistory'
+                       ' WHERE [TagName] = \'Converter5_Q\'  AND [DateTime] > DATEADD(mi, -2, GetDate()) AND'
+                       '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\') as c5_q,'
+                       '(SELECT AVG([Value]) FROM Runtime.dbo.v_AnalogHistory'
+                       ' WHERE [TagName] = \'Converter6_Q\'  AND [DateTime] > DATEADD(mi, -2, GetDate()) AND'
+                       '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\') as c6_q,'
+                       '(SELECT AVG([Value]) FROM Runtime.dbo.v_AnalogHistory'
+                       ' WHERE [TagName] = \'Converter7_Q\'  AND [DateTime] > DATEADD(mi, -2, GetDate()) AND'
+                       '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\') as c7_q,'
+                       '(SELECT AVG([Value]) FROM Runtime.dbo.v_AnalogHistory'
+                       ' WHERE [TagName] = \'Converter8_Q\'  AND [DateTime] > DATEADD(mi, -2, GetDate()) AND'
+                       '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\') as c8_q,'
+                       '(SELECT AVG([Value]) FROM Runtime.dbo.v_AnalogHistory'
+                       ' WHERE [TagName] = \'Flue_SO2_Molodejniy\'  AND [DateTime] > DATEADD(mi, -2, GetDate()) AND'
+                       '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\') as SO2_M,'
+                       '(SELECT AVG([Value]) FROM Runtime.dbo.v_AnalogHistory'
+                       ' WHERE [TagName] = \'Flue_SO2_Nagornaya\'  AND [DateTime] > DATEADD(mi, -2, GetDate()) AND'
+                       '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\') as SO2_N,'
+                       '(SELECT AVG([Value]) FROM Runtime.dbo.v_AnalogHistory'
+                       ' WHERE [TagName] = \'Flue_SO2_UGMS\'  AND [DateTime] > DATEADD(mi, -2, GetDate()) AND'
+                       '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\') as SO2_UG,'
+                       '(SELECT TOP 1 CAST([Value] as int) & 67108864 FROM Runtime.dbo.v_AnalogHistory'
+                       ' WHERE [TagName] = \'ShiberLocalRemote_RTP3\'  AND'
+                       '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\' order by '
+                       '[DateTime] desc) as RTP3,'
+                       '(SELECT TOP 1 CAST([Value] as int) & 67108864 FROM Runtime.dbo.v_AnalogHistory'
+                       ' WHERE [TagName] = \'ShiberLocalRemote_RTP4\'  AND'
+                       '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\' order by '
+                       '[DateTime] desc) as RTP4,'
+                       '(SELECT TOP 1 CAST([Value] as int) & 1 FROM Runtime.dbo.v_AnalogHistory'
+                       ' WHERE [TagName] = \'P5_mess_Furnace\' AND'
+                       '[DateTime] <= GetDate() AND  wwResolution = 1000 AND wwRetrievalMode = \'CYCLIC\' order by '
+                       '[DateTime] desc) as RTP5')
         return cursor.fetchall()
 
 
@@ -62,16 +62,41 @@ class SqlLiteBase(object):
         try:
             con = lite.connect(self.base)
             cur = con.cursor()
-            cur.execute('insert into sovisu_analizatordata (an_date, '
-                                    'so_m, so_n, so_ug, so_n_date, '
-                                    'so_m_date, so_ug_date, so_m_nr, '
-                                    'so_n_nr, so_ug_nr, so_m_nr_v, '
-                                    'so_n_nr_v, o_ug_nr_v)'
+            cur.execute('INSERT INTO sovisu_analizatordata (an_date, '
+                        'so_m, so_n, so_ug, so_n_date, '
+                        'so_m_date, so_ug_date, so_m_nr, '
+                        'so_n_nr, so_ug_nr, so_m_nr_v, '
+                        'so_n_nr_v, o_ug_nr_v)'
                         'VALUES (:an_date,:so_m, :so_n, :so_ug, :so_n_date, '
-                                    ':so_m_date, :so_ug_date, :so_m_nr, '
-                                    ':so_n_nr, :so_ug_nr, :so_m_nr_v, '
-                                    ':so_n_nr_v, :so_ug_nr_v)', data)
+                        ':so_m_date, :so_ug_date, :so_m_nr, '
+                        ':so_n_nr, :so_ug_nr, :so_m_nr_v, '
+                        ':so_n_nr_v, :so_ug_nr_v)', data)
 
+        except lite.DatabaseError as err:
+            print("Error: ", err)
+
+        finally:
+            if con:
+                con.close()
+
+    def writewheterdata(self, data):
+        try:
+            con = lite.connect(self.base)
+            cur = con.cursor()
+            c = cur.execute("""SELECT EXISTS (SELECT 1 
+                                             FROM sovisu_weather
+                                             WHERE wth_date=?
+                                             LIMIT 1)""", (data['wth_date'],)).fetchone()[0]
+
+            if not c:
+                cur.execute('INSERT INTO sovisu_weather (T, '
+                            'Po, P, U, ff10, '
+                            'ff3, Td, RRR, '
+                            'Wg, wth_date)'
+                            'VALUES (:T,:Po, :P, :U, :ff10, '
+                            ':ff3, :Td, :RRR, '
+                            ':Wg, :wth_date)', data)
+                con.commit()
         except lite.DatabaseError as err:
             print("Error: ", err)
 
