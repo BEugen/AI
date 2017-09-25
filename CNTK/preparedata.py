d9acfb0d48b1c68f3d5133a3b528f16ed992adca
BattsEA <b-eugen@yandex.ru>
2017-09-25 18:33:41 +0300

    Create learn

--- CNTK/preparedata.py
+++ CNTK/preparedata.py
@@ -1,12 +1,18 @@
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-
+from DATA import datasql
 import pandas as pd
 
 so = pd.read_csv('data_so2N.csv', delimiter=';')
-print(so)
 sc_feat = so.copy()
 col_n = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 sc_feat[col_n] = \
     MinMaxScaler().fit_transform(sc_feat[col_n].as_matrix())
 sc_feat.to_csv('dt_so2n.csv', sep=';')
+sql = datasql.SqlLiteBase('/home/administrator/projects/websovisu/db.sqlite3')
+data_full = sql.getwheterdata()
+so = pd.DataFrame(data_full)
+so[col_n] = \
+    MinMaxScaler().fit_transform(so[col_n].as_matrix())
+so.to_csv('dt_so2n-p.csv', sep=';')
+
 
