8773d7b7f1943388e1625cccb2a46425eba546e6
eugen <b-eugen@yandex.ru>
2017-09-30 10:46:03 +0300

    cntk test

--- CNTK/CNTKCassification.py
+++ CNTK/CNTKCassification.py
@@ -8,10 +8,11 @@ from cntk.device import *
 import pylab
 import numpy as np
 import pandas as pd
+from DATA import fakedata
 
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
-
+fd = fakedata.FackeData()
 class CntkClassification(object):
     def __init__(self, model_path):
         self.model_path = model_path
@@ -19,8 +20,11 @@ class CntkClassification(object):
     def evaluate(self, data):
         z = load_model(self.model_path)
         so = pd.DataFrame(data)
+        so.append(fd.maxdata1())
+        so.append(fd.mindata1())
         so.iloc[:, :11] = \
             MinMaxScaler().fit_transform(so.iloc[:, :11].as_matrix())
+        print(so)
         features = np.ascontiguousarray(so.iloc[:1, :11], dtype=np.float32)
         output = z.eval({z.arguments[0]: [features]})
         top_class = np.argmax(output)
